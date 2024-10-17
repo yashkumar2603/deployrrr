0e81717dbc99718ac6d0c5534d8d12f3206277f8
yashkumar2603 <yash.2603kumar@gmail.com>
2024-10-18 02:29:26 +0530

    Reverse Proxy setup instead of GetObject

---
+++ reverse-proxy/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const httpProxy = require('http-proxy');
+
+const app = express();
+const PORT = 3000;
+
+const BASE_PATH = 'https://deployerrr-outputs.s3.us-east-1.amazonaws.com/dist';
+
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
+
+    // Construct the target URL
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+    // Proxy the request
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+});
+
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        proxyReq.path += 'index.html'; // Add 'index.html' if the request is for the root
+    }
+});
+
+// Add a response interceptor to set the correct Content-Type header
+proxy.on('proxyRes', (proxyRes, req, res) => {
+    if (proxyRes.headers['content-type']) {
+        res.set('Content-Type', proxyRes.headers['content-type']);
+    }
+});
+
+app.listen(PORT, () => console.log(`Reverse Proxy Running on port ${PORT}`));
